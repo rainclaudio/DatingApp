8b1fa8d769c7eb1d39b566642a6f27f57dfdf2e0
rainclaudio <rainclaudio25@gmai.com>
2023-11-20 10:54:39 -0300

    Creating database using EF Framework

---
+++ API/-- Users.sql
@@ -0,0 +1,14 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Manuel");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Tom");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Philip");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4,"Mark");
+
+
