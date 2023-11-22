aa58e5106551ea23b28314c7b212c1f76f9e2377
rainclaudio <rainclaudio25@gmai.com>
2023-11-22 16:57:04 -0300

    displaying users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dating app';
   // any could be anything, so is technically cheating
   users:any = [];
@@ -15,6 +15,16 @@ export class AppComponent {
   // private means use only in the class
   constructor(private http: HttpClient){
 
+  }
+  ngOnInit(): void {
+    // no se ejecutará a menos que alguien observe esos cambios
+    // por lo tanto, necesitamos subscribirnos a ellos 
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      // next: lo que queremos que suceda luego de obtener los datos
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request as completed')
+    })
   }
 
 
