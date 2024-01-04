3487739a7313aeb47884f56e7590cc658726cc2b
rainclaudio <rainclaudio25@gmai.com>
2024-01-04 12:18:37 -0300

    Login functionallity - part 1

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -23,7 +23,7 @@ public class AccountController : BaseApiController
   public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
   {
     if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-   
+
     // to hash password we'll use HMAC512
     // we are going to use this as salt
     using var hmac = new HMACSHA512();
@@ -41,6 +41,28 @@ public class AccountController : BaseApiController
 
     return user;
   }
+
+  [HttpPost("login")]
+  public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+  {
+    var user = await _context.Users.FirstOrDefaultAsync(x => 
+    x.UserName == loginDto.Username);
+
+    if(user == null) return Unauthorized("Invalid username");
+
+    using var hmac = new HMACSHA512(user.PasswordSalt);
+
+    var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+    for (int i = 0; i < computedHash.Length; i++)
+    {
+      if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+    }
+
+
+  }
+
+
 
   private async Task<bool> UserExists(string username)
   {
