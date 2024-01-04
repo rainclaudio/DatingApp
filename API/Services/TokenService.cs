e817551776e8f289618c66bacf7877200b6f76b1
rainclaudio <rainclaudio25@gmai.com>
2024-01-04 13:10:20 -0300

    implementing Create token service

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,14 +1,44 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        // able to interpret the token      
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config)
+        {
+          _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List <Claim>
+            {
+              new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+              Subject = new ClaimsIdentity(claims),
+              Expires = DateTime.Now.AddDays(7),
+              SigningCredentials = creds
+            };
+
+            var TokenHandler = new JwtSecurityTokenHandler();
+
+            var token = TokenHandler.CreateToken(tokenDescriptor);
+
+            return TokenHandler.WriteToken(token);
         }
     }
 }
